ee4aeb754766542d2b5f34dd51479afab16f7eaa
EFDRON <157123352+EFDRON@users.noreply.github.com>
2025-06-08 02:23:20 +0400

    Building Game Card

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,16 @@
+import { Card, Heading, Image } from "@chakra-ui/react";
+import useGames, { type Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image}></Image>
+      <Heading fontSize="2xl">{game.name}</Heading>
+    </Card>
+  );
+};
+
+export default GameCard;

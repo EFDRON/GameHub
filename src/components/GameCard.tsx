c9b031a74532ef9ad78a0c74a458146966a5d8ee
EFDRON <157123352+EFDRON@users.noreply.github.com>
2025-06-10 02:30:47 +0400

    Show Loading Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   console.log(getCroppedImageUrl(game.background_image));
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <Heading fontSize="2xl">{game.name}</Heading>
       <HStack justifyContent="space-between" padding={2}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,12 +22,15 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     const controller = new AbortController();
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
@@ -36,7 +39,7 @@ const useGames = () => {
       });
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;

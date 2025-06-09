361c932f811f2d9689d20a9248cbce436df4152b
EFDRON <157123352+EFDRON@users.noreply.github.com>
2025-06-10 01:45:14 +0400

    Displaying platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,14 +1,20 @@
-import { Card, Heading, Image } from "@chakra-ui/react";
+import { Card, Heading, Image, Text } from "@chakra-ui/react";
 import useGames, { type Game } from "../hooks/useGames";
+import PlatformiconList from "./PlatformiconList";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
+  console.log(game.parent_platforms.map((platform) => platform.platform));
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <Heading fontSize="2xl">{game.name}</Heading>
+
+      <PlatformiconList
+        platform={game.parent_platforms.map((platform) => platform.platform)}
+      ></PlatformiconList>
     </Card>
   );
 };

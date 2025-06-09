92f8bf397ec67bd1cf7d984011bbdb578e717640
EFDRON <157123352+EFDRON@users.noreply.github.com>
2025-06-10 02:12:31 +0400

    Cropped the Images(optimized Images)

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,15 +2,16 @@ import { Card, Heading, HStack, Image } from "@chakra-ui/react";
 import { type Game } from "../hooks/useGames";
 import PlatformiconList from "./PlatformiconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
-  console.log(game.parent_platforms.map((platform) => platform.platform));
+  console.log(getCroppedImageUrl(game.background_image));
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <Heading fontSize="2xl">{game.name}</Heading>
       <HStack justifyContent="space-between" padding={2}>
         <PlatformiconList
